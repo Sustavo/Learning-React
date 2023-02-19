9eaad92aad44f378a0dfa74564cda2c174cbb584
Gustavo <gust4v0309@gmail.com>
2023-02-18 21:42:31 -0300

    feat: using fetch in useEffect

--- hooks/src/components/useEffect/UseEffect1.jsx
+++ hooks/src/components/useEffect/UseEffect1.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react"
 
 export default function UseEffect1() {
-    const [contar, setContar] = useState(1);
-
-    const handleClick = () => {
-        setContar((contar) => contar + 1)
-    }
-
+    const [contar, setContar] = useState(0);
+    const [dados, setDados] = useState(null);
 
     useEffect(() => {
-        console.log('executou')
-        document.title = 'Total' + contar
-    }, [contar])
+        fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
+        .then((response) => response.json())
+        .then((json) => setDados(json));
+
+    }, [])
+    console.log(dados)
 
     return(
         <div>
+            {dados && (
+                <div>
+                    <h1>{dados.nome}</h1>
+                    <p>R$ {dados.preco * contar}</p>
+                </div>
+            )}
             <button style={{padding: '10px'}} onClick={() => setContar(contar + 1)}>{contar}</button>
         </div>
     )

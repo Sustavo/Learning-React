39d2bae3ee24b802f3b90e07aa6dd1f1f6ea361d
Gustavo <gust4v0309@gmail.com>
2023-02-18 21:24:16 -0300

    Feat: finalize exercise UseState

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,10 +1,11 @@
 import UseState1 from './components/UseState/UseState1.jsx'
+import UseEffect1 from './components/useEffect/UseEffect1.jsx'
 
 
 function App() {
   return (
     <div className="App">
-      <UseState1 />
+      <UseEffect1 />
     </div>
   );
 }
--- hooks/src/components/UseState/UseState1.jsx
+++ hooks/src/components/UseState/UseState1.jsx
@@ -1,38 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function UseState1() {
-    const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState(null);
 
-    const fetchApi = async (valor) => {
-        try {
-            const response = await fetch(
-                `https://ranekapi.origamid.dev/json/api/produto/${valor}`
-            ).then((response) => response.json());
+  const fetchApi = async (valor) => {
+    try {
+      const response = await fetch(
+        `https://ranekapi.origamid.dev/json/api/produto/${valor}`
+      ).then((response) => response.json());
 
-            setProduct(response);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      setProduct(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-
-
-    return (
-        <div style={{ display: "flex", flexDirection: "column" }}>
-            <button onClick={() => fetchApi('tablet')}>tablet</button>
-            <button onClick={() => fetchApi('smartphone')}>smartphone</button>
-            <button onClick={() => fetchApi('notebook')}>notebook</button>
-            {product && (
-                <ul>
-                    <li>{product.descricao}</li>
-                    <li>{product.id}</li>
-                    <li>{product.nome}</li>
-                    <li>{product.preco}</li>
-                    <li>{product.usuario}</li>
-                </ul>
-            )}
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <button onClick={() => fetchApi("tablet")}>tablet</button>
+      <button onClick={() => fetchApi("smartphone")}>smartphone</button>
+      <button onClick={() => fetchApi("notebook")}>notebook</button>
+      {product && (
+        <ul>
+          <li>{product.descricao}</li>
+          <li>{product.id}</li>
+          <li>{product.nome}</li>
+          <li>{product.preco}</li>
+          <li>{product.usuario}</li>
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default UseState1;

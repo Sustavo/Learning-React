2bdbf7e6aba0eefddb22eca0f569901cfed20490
Gustavo <gust4v0309@gmail.com>
2023-02-22 12:00:56 -0300

    Feat: Create useLocalStorage to save and create key and value

---
+++ hooks/src/components/customHooks/useLocalStorage/useLocalStorage.jsx
@@ -0,0 +1,16 @@
+const { useState, useEffect } = require("react")
+
+const useLocalStorage = (key, inicial) => {
+    const[state, setState] = useState(() => {
+        const local = window.localStorage.getItem(key);
+        return local ? local : inicial;
+    });
+
+    useEffect(() => {
+        window.localStorage.setItem(key, state);
+    }, [state, key])
+
+    return [state, setState];
+}
+
+export default useLocalStorage;

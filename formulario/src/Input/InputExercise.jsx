2ea00ef69edf2894fd1f14b6d566da179212dc2e
Gustavo <gust4v0309@gmail.com>
2023-02-22 19:33:06 -0300

    Feat: Exercise using Input, make a good code

---
+++ formulario/src/Input/InputExercise.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import "./style.css";
+
+const formFields = [
+  {
+    id: "nome",
+    label: "Nome",
+    type: "text",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+  },
+  {
+    id: "senha",
+    label: "Senha",
+    type: "password",
+  },
+  {
+    id: "cep",
+    label: "Cep",
+    type: "text",
+  },
+  {
+    id: "rua",
+    label: "Rua",
+    type: "text",
+  },
+  {
+    id: "numero",
+    label: "Numero",
+    type: "text",
+  },
+  {
+    id: "bairro",
+    label: "Bairro",
+    type: "text",
+  },
+  {
+    id: "cidade",
+    label: "Cidade",
+    type: "text",
+  },
+  {
+    id: "estado",
+    label: "Estado",
+    type: "text",
+  },
+];
+
+export default function InputExercise() {
+  const [form, setForm] = useState(
+    formFields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.id]: "",
+      };
+    }, {})
+  );
+
+  const [response, setResponse] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch(
+      "https://ranekapi.origamid.dev/json/api/usuario",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      }
+    );
+    setResponse(response);
+  };
+
+  const handleChange = ({ target }) => {
+    const { id, value } = target;
+    setForm((prevForm) => ({ ...prevForm, [id]: value }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {formFields.map(({ id, label, type }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input
+            type={type}
+            id={id}
+            value={form[id]}
+            onChange={handleChange}
+          />
+        </div>
+      ))}
+      {response?.ok && <p>Formulário Enviado</p>}
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}

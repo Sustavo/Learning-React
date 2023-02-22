c7154f5af3d2d86233248d29c3fa1aca5ba40891
Gustavo <gust4v0309@gmail.com>
2023-02-22 19:23:18 -0300

    APP

--- formulario/src/App.jsx
+++ formulario/src/App.jsx
@@ -1,8 +1,10 @@
+import Input1 from "./Input/Input1";
+import InputExercise from "./Input/InputExercise";
 
 function App() {
   return (
     <div className="App">
-
+      <InputExercise />
     </div>
   );
 }

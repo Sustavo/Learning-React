e608978b6598d416f223d5a1650d5d7ea259e0d6
Gustavo <gust4v0309@gmail.com>
2023-02-24 14:09:31 -0300

    App

--- formulario/src/App.jsx
+++ formulario/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import CheckBox from "./CheckBox/CheckBox";
+import CheckBoxExercise from "./CheckBox/CheckBoxExercise";
+import InputComponent from "./Components/Input/InputComponent";
 import Input1 from "./Input/Input1";
 import InputExercise from "./Input/InputExercise";
+import Radio from "./Radio/Radio";
+import Select from "./Select/Select";
+import TextArea from "./textArea/TextArea";
+import InputStructure from "./Components/Input/InputStructure";
+import SelectStructure from "./Components/Select/SelectStructure";
+import RadioStructure from "./Components/Radio/RadioStructure";
+import RadioComponent from "./Components/Radio/RadioComponent";
+import CheckBoxComponent from "./Components/CheckBox/CheckBoxComponent";
 
 function App() {
   return (
     <div className="App">
-      <InputExercise />
+      <CheckBoxComponent />
     </div>
   );
 }

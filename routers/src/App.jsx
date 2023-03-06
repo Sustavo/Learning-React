386291662eee3c57aaba75a26430d6d7f7ad2988
Gustavo <gust4v0309@gmail.com>
2023-03-06 20:41:26 -0300

    Feat: learning Routes

--- routers/src/App.jsx
+++ routers/src/App.jsx
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Sobre from './components/sobre/Sobre';
+import Header from './components/header/Header';
+import Login from './components/login/Login';
+import Notfound from './components/404/Notfound';
+import Produto from './components/Produto/Produto';
+import ProdutoDescricao from "./components/Produto/ProdutoDescricao";
+import ProdutoAvaliacao from "./components/Produto/ProdutoAvaliacao";
+import ProdutoCustomizado from "./components/Produto/ProdutoCustomizado";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="login" element={<Login />} />
+        <Route path="produto/:id" element={<Produto />}>
+          <Route path="/" element={<ProdutoDescricao />} />
+          <Route path="avaliacao" element={<ProdutoAvaliacao />} />
+          <Route path="customizado" element={<ProdutoCustomizado />} />
+        </Route>
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
